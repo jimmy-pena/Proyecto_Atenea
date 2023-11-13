411c7914397b306c858fe9ded46893c41041ac0b
Jimmy Armando Peña Gonzalez <jimmy.pena@viamericas.com>
2023-11-13 14:25:50 -0500

    proyecto JCJ

---
+++ index.php
@@ -0,0 +1,116 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="keywords" content="abogados, litigio, litigar, jurisprudencia, penal, abogado, asesoria">
+    <meta name="author" content="Jimmy Peña">
+    <meta name="description" content="Aplicación para que los clientes pongan su caso y sean constactados por los abogados">
+    <script src="https://kit.fontawesome.com/bf340f2abf.js" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="css/style_main.css">
+    <title>JCJ CONSULT LAWYERS</title>
+</head>
+
+<body>
+    <header>
+        <img src="imgs/LOGO-PhotoRoom.png-PhotoRoom.png" alt="">
+        <nav class="site-nav">
+            <ul>
+                <li><a href="#">Inicio</a></li>
+                <li><a href="#">Quiénes Somos</a></li>
+                <li><a href="#">Servicios</a></li>
+                <li><a href="#">Equipo</a></li>
+                <li><a href="#">Ingresar</a></li>
+            </ul>
+        </nav>
+    </header>
+    <section class="main-text">
+        <div class="main-containt">
+            <div class="one-text">
+                <p>Nosotros somos expertos 
+                    en temas de Litigio 
+                </p>
+            </div>
+            <div class="second-text">
+                <p>Con más de 5 años de experiencia en el ámbito jurídico</p>
+            </div>
+            <div class="main-button">
+                <button type="button" id="btn">Cuéntanos tu caso</button>
+            </div>
+        </div>
+    </section>
+    <div class="separator">
+        <span class="line-separator"></span>
+        
+        <img src="imgs/separador.png" class="image-separator">
+
+        <span class="line-separator"></span>
+    </div>
+
+    <section class="specialties">
+        <div class="tittle-specialties">
+            <h2>
+                <p><span class="middle-text">Los mejores Especialistas</span> a tu disposición
+                </p>
+            </h2>
+        </div>
+        <div class="container-specialties">
+            <div class="container-1">
+                <img src="imgs/glaboral.png" alt="">
+            </div>
+            <div class="container-1">
+                <img src="imgs/gcomercial.png" alt="">
+            </div>
+            <div class="container-1">
+                <img src="imgs/gpenal.png" alt="">
+            </div>
+            <div class="container-1">
+                <img src="imgs/gcivil.png" alt="">
+            </div>
+        </div>
+    </section>
+    
+    <footer id="contact">
+        <div class="contain-contact">
+            <div class="info-contact">
+                <h4>Contáctenos</h4>
+                <address>
+                    <dl class="text-information">
+                        <dt class="fa-solid fa-phone" style="color: #c19e2d";>
+                        <span class="cel-phone">
+                            320 447 4488   305 790 6271
+                        </span></dt>
+                        <dt class="fa-solid fa-envelope" style="color: #c19e2d";>
+                        <span class="email">
+                            jcjconsultlawyers@gmail.com
+                        </span></dt>
+                    </dl>
+                </address>
+            </div>
+            <div class="info-location">
+                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2364.7122363946282!2d-74.07892959465084!3d4.601887849876817!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8e3f99990deca339%3A0x6f69f047e257de51!2sCl.%2012b%20%2310-41%2C%20Bogot%C3%A1!5e0!3m2!1ses!2sco!4v1699893621633!5m2!1ses!2sco"
+                    allowfullscreen="" 
+                    loading="lazy" 
+                    referrerpolicy="no-referrer-when-downgrade">
+                </iframe>
+                <p>
+                    <span class="fa-solid fa-location-dot" style="color: #c19e2d";></span>
+                    <span class="addres">calle 12b #10 - 41 Oficina 907</span>
+                </p>
+            </div>
+            <div class="info-social">
+                <h4>Encuéntranos</h4>
+                <div class="social-media">
+                <i class="fa-brands fa-square-x-twitter"></i>
+                <i class="fa-brands fa-linkedin"></i>
+                <i class="fa-brands fa-facebook"></i>
+                <i class="fa-brands fa-instagram"></i>
+                </div>
+            </div>
+        </div>
+    </footer>
+</body>
+
+</html>
